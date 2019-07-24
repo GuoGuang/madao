679954605498e7b8d7728ccd15d1343a371b5dff
guoguang0536@gmail.com <guoguang0536@gmail.com>
2019-07-24 18:57:42 +0800

    Update icon & upgrade element-ui

--- pages/article/mockdata.js
+++ pages/article/mockdata.js
@@ -9,7 +9,7 @@ const comment = {
       tweetsId: 'talents100020', // 吐槽的id
       userId: 'errhefe232213', // 评论者id
       userName: '犀利的评论家', // 评论者昵称
-      avatar: 'http://ww4.sinaimg.cn/bmiddle/006DLFVFgy1ft0j2pddjuj30v90uvagf.jpg', // 评论者头像
+      avatar: 'https://images.nowcoder.com/images/20180218/6617757_1518920311404_48DBFD0E780C1F7DCB9ABC4D5083B2BD@0e_100w_100h_0c_1i_1o_90Q_1x', // 评论者头像
       likeNum: 3, // 点赞人数
       content: '非常靠谱的程序员', // 评论内容
       reply: [ // 回复，或子评论
@@ -18,10 +18,10 @@ const comment = {
           commentId: 'comment0001', // 父评论id，即父亲的id
           userId: 'observer223432', // 评论者id
           userName: '夕阳红', // 评论者昵称
-          avatar: 'https://wx4.sinaimg.cn/mw690/69e273f8gy1ft1541dmb7j215o0qv7wh.jpg', // 评论者头像
+          avatar: 'https://user-gold-cdn.xitu.io/2019/3/4/16947f5e8bd5719f?imageView2/1/w/100/h/100/q/85/format/webp/interlace/1', // 评论者头像
           toId: 'errhefe232213', // 被评论者id
           toName: '犀利的评论家', // 被评论者昵称
-          toAvatar: 'http://ww4.sinaimg.cn/bmiddle/006DLFVFgy1ft0j2pddjuj30v90uvagf.jpg', // 被评论者头像
+          toAvatar: 'https://images.nowcoder.com/images/20180218/6617757_1518920311404_48DBFD0E780C1F7DCB9ABC4D5083B2BD@0e_100w_100h_0c_1i_1o_90Q_1x', // 被评论者头像
           content: '赞同，很靠谱，水平很高', // 评论内容
           createAt: '2018-07-05 08:35' // 评论时间
         },
@@ -33,7 +33,7 @@ const comment = {
           avatar: 'http://imgsrc.baidu.com/imgad/pic/item/c2fdfc039245d688fcba1b80aec27d1ed21b245d.jpg',
           toId: 'observer223432',
           toName: '夕阳红',
-          toAvatar: 'https://wx4.sinaimg.cn/mw690/69e273f8gy1ft1541dmb7j215o0qv7wh.jpg',
+          toAvatar: 'https://user-gold-cdn.xitu.io/2019/3/4/16947f5e8bd5719f?imageView2/1/w/100/h/100/q/85/format/webp/interlace/1',
           content: '大神一个！',
           createAt: '2018-07-05 08:50'
         }
@@ -45,7 +45,7 @@ const comment = {
       tweetsId: 'talents100020',
       userId: 'errhefe232213',
       userName: '毒蛇郭德纲',
-      avatar: 'http://ww1.sinaimg.cn/bmiddle/006DLFVFgy1ft0j2q2p8pj30v90uzmzz.jpg',
+      avatar: 'https://images.nowcoder.com/images/20180102/63_1514861814371_E573EC9DA05DFAC7340D94F1A6D246E3@0e_100w_100h_0c_1i_1o_90Q_1x',
       likeNum: 0,
       content: '从没见过这么优秀的人',
       reply: []
