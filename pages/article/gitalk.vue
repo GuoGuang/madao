896061fcb63bd6855ec5b6f93baea94684a3f434
guoguang0536@gmail.com <guoguang0536@gmail.com>
2019-08-01 14:54:42 +0800

    :art: Add Gitalk plugin

---
+++ pages/article/gitalk.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="gitalk-container">
+    <div id="gitalk-container"/>
+  </div>
+</template>
+<script>
+import 'gitalk/dist/gitalk.css'
+import Gitalk from 'gitalk'
+export default {
+  name: 'Gitalk',
+  data() {
+    return {}
+  },
+  mounted() {
+    const gitalk = new Gitalk({
+      clientID: 'GitHub Application Client ID',
+      clientSecret: 'GitHub Application Client Secret',
+      repo: 'GitHub repo',
+      owner: 'GitHub repo owner',
+      admin: ['GitHub repo owner and collaborators, only these guys can initialize github issues'],
+      id: location.pathname, // Ensure uniqueness and length less than 50
+      distractionFreeMode: false // Facebook-like distraction free mode
+    })
+
+    gitalk.render('gitalk-container')
+  }
+}
+</script>
+
