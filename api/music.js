621806cd56ef431f07e2d4c7098204a03064b64b
guoguang0536@gmail.com <guoguang0536@gmail.com>
2019-09-15 23:50:06 +0800

    :sparkles: Add new music api

--- api/music.js
+++ api/music.js
@@ -1,10 +1,32 @@
 import request from '@/utils/request'
 
+const SERVICE_BASE = '/api/base'// base服务
+
 export function getMusicList() {
   return request({
     url: '/music',
     method: 'get'
+  })
+}
 
+export function getMusicURL(id) {
+  return request({
+    url: `/music/url/${id}`,
+    method: 'get'
+  })
+}
+
+export function getMusicDetail(id) {
+  return request({
+    url: SERVICE_BASE + '/music/song/' + id,
+    method: 'get'
+  })
+}
+
+export function getMusicLyric(id) {
+  return request({
+    url: SERVICE_BASE + `/music/${id}/lyric`,
+    method: 'get'
   })
 }
 
