f0544eb3ff111d5434032519e15af5fdd656ce6f
guoguang0536@gmail.com <guoguang0536@gmail.com>
2019-12-08 14:54:48 +0800

    :sparkles: Add new bullet screen style

--- components/widget/barrage/item.vue
+++ components/widget/barrage/item.vue
@@ -1,17 +1,29 @@
 <template>
-  <li
-    :style="styles"
-    :class="[
-      `size-${barrage.style.size}`,
-      `color-${barrage.style.color}`,
-      playing ? 'playing' : '',
-      played ? 'played' : ''
-    ]"
-    class="barrage-item"
-  >
-    <span class="gravatar"/>
-    <span class="content" v-text="barrage.text"/>
-  </li>
+  <div>
+    <li
+      :style="styles"
+      :class="[
+        `size-${barrage.style.size}`,
+        `color-${barrage.style.color}`,
+        playing ? 'playing' : '',
+        played ? 'played' : '',
+        'eKOLup'
+      ]"
+      class="barrage-item"
+    >
+      <div class="head">
+        <img
+          :src="heads[Math.floor(Math.random() * heads.length)].path"
+          alt="msg head"
+        >
+      </div>
+      <span class="gravatar" />
+      <span
+        class="content msg"
+        v-text="barrage.text"
+      />
+    </li>
+  </div>
 </template>
 
 <script>
@@ -44,7 +56,49 @@ export default {
       styles: {
         'z-index': (this.id + 1) * 10,
         transition: `transform ${this.delay}s linear`
-      }
+      },
+      heads: [
+        {
+          title: '奥特曼',
+          path: require('assets/images/aotuman.jpg')
+        },
+        {
+          title: '熊孩子',
+          path: require('assets/images/bear.jpg')
+        },
+        {
+          title: '卡通女孩',
+          path: require('assets/images/carton.girl.jpg')
+        },
+        {
+          title: '卡通男孩',
+          path: require('assets/images/carton.jpg')
+        },
+        {
+          title: '猫咪',
+          path: require('assets/images/cat.jpg')
+        },
+        {
+          title: '小老弟',
+          path: require('assets/images/dog.jpg')
+        },
+        {
+          title: '可爱',
+          path: require('assets/images/girl.jpg')
+        },
+        {
+          title: '蜡笔小新',
+          path: require('assets/images/xiaoxin.jpg')
+        },
+        {
+          title: '气质美女',
+          path: require('assets/images/nv2.jpg')
+        },
+        {
+          title: '小猫',
+          path: require('assets/images/xiongmao.gif')
+        }
+      ]
     }
   },
   ready() {
@@ -69,8 +123,8 @@ export default {
       const innerCount = innerHeight / 30
       let randomCount = this.$parent.randomPer(innerCount) - 3
       randomCount = randomCount < 0 ? 1 : randomCount
-      let topPre = randomCount / innerCount * 100
-      topPre = (topPre > 88) ? 86 : topPre
+      let topPre = (randomCount / innerCount) * 100
+      topPre = topPre > 88 ? 86 : topPre
       return `${parseInt(topPre, 0)}%`
     },
     startAnimation() {
@@ -97,19 +151,62 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-  .barrage-item {
-    width: auto;
-    right: 0px;
-    display: block;
-    position: absolute;
-    transform: translate3d(100%, 0, 0);
-
-    &.playing {
-      transform: translate3d(calc(-100vw - 200%), 0, 0);
-    }
-
-    &.played {
-      visibility: hidden;
-    }
+.eKOLup {
+  background-color: rgba(243, 249, 241, 0.8);
+  position: relative;
+  box-shadow: rgba(0, 0, 0, 0.5) 0px 0px 10px;
+  word-break: keep-all;
+  white-space: pre-wrap;
+  padding: 8px 18px;
+  border-width: 2px;
+  border-style: solid;
+  border-color: rgb(255, 255, 255);
+  border-image: initial;
+  border-radius: 26px;
+}
+.head::after {
+  position: absolute;
+  top: 50%;
+  right: -14px;
+  transform: translateY(-50%);
+  content: '';
+  display: block;
+  width: 8px;
+  height: 2px;
+  background: rgb(255, 255, 255);
+}
+.head {
+  img {
+    width: 100%;
+    height: 100%;
+    border-radius: 50%;
   }
+  position: absolute;
+  left: -58px;
+  top: 50%;
+  transform: translateY(-50%);
+  width: 40px;
+  height: 40px;
+  box-shadow: rgba(0, 0, 0, 0.8) 0px 0px 10px;
+  border-radius: 50%;
+  border-width: 2px;
+  border-style: solid;
+  border-color: rgb(238, 238, 238);
+  border-image: initial;
+}
+.barrage-item {
+  width: auto;
+  right: 0px;
+  display: block;
+  position: absolute;
+  transform: translate3d(100%, 0, 0);
+
+  &.playing {
+    transform: translate3d(calc(-100vw - 200%), 0, 0);
+  }
+
+  &.played {
+    visibility: hidden;
+  }
+}
 </style>
