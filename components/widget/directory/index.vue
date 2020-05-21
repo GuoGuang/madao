456190e096dbcca3c9cfa0b53aec9537fceee1bc
guoguang0536@gmail.com <guoguang0536@gmail.com>
2020-05-21 22:49:10 +0800

    :beers: Writing code drunkenly.

--- components/widget/directory/index.vue
+++ components/widget/directory/index.vue
@@ -23,7 +23,7 @@ export default {
     // 使用演示5ms解决异步获取数据后document.getElementById(Opt.contentEl)为null的问题
     setTimeout(() => {
       const defaultOpts = {
-        selector: ['h1', 'h2', 'h3', 'h4', 'h5', 'h6'] // 按优先级排序
+        selector: ['h1', 'h2', 'h3'] // 按优先级排序
       }
       const Opt = Object.assign({}, defaultOpts, {
         contentEl: 'article-content',
