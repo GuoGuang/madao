b29e6ca74c984620de9a3a8bb3d94e61125e9725
guoguang0536@gmail.com <guoguang0536@gmail.com>
2020-06-13 14:41:48 +0800

    :sparkles: Introducing timeline

---
+++ pages/timeLine.vue
@@ -0,0 +1,57 @@
+<template>
+  <div style="justify-content: center;display: flex;margin-top: 20px;">
+    <el-collapse-transition>
+      <el-timeline>
+        <el-timeline-item
+          v-for="(activity, index) in activities"
+          :key="index"
+          :icon="activity.icon"
+          :type="activity.type"
+          :color="activity.color"
+          :size="activity.size"
+          :timestamp="activity.timestamp">
+          {{ activity.content }}
+        </el-timeline-item>
+      </el-timeline>
+    </el-collapse-transition>
+  </div>
+
+</template>
+
+<script>
+export default {
+  name: 'TimeLine',
+  data() {
+    return {
+      activities: [{
+        content: '网站上线',
+        timestamp: '2019-11-30 20:46',
+        size: 'large',
+        type: 'primary',
+        icon: 'el-icon-more'
+      }, {
+        content: '新增"开源"板块',
+        timestamp: '2019-12-03',
+        color: '#0bbd87'
+      }, {
+        content: '新增"音乐"子板块',
+        timestamp: '2020-01-30',
+        color: '#0bbd87'
+      }, {
+        content: '新增"福利"板块',
+        timestamp: '2020-03-24',
+        color: '#0bbd87'
+      }, {
+        content: '新增"服务"板块',
+        timestamp: '2020-06-13',
+        color: '#0bbd87'
+      }]
+    }
+  },
+  computed: {
+  },
+  methods: {
+
+  }
+}
+</script>
