3248377a42f7d5207c11bde89428145f828fb8c5
guoguang0536@gmail.com <guoguang0536@gmail.com>
2020-07-02 22:01:22 +0800

    :zap: optimize SEO

--- config/app.config.js
+++ config/app.config.js
@@ -6,7 +6,7 @@
 
 exports.meta = {
   title: 'CODEWAY',
-  keywords: 'codeway,后端,java开发,博客,个人网站，SpringCloud,架构师',
+  keywords: 'codeway,后端,java开发,博客,个人网站，SpringCloud,架构师,码道人生,码道',
   description: '百学须先立志',
   url: 'codeway.fun',
   author: 'codeway',
