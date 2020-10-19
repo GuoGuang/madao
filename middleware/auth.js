886ba45a9e1f5555a0d3bb42f46a1b582f846b10
guoguang0536@gmail.com <guoguang0536@gmail.com>
2020-10-19 23:45:35 +0800

    :wrench: 添加授权功能

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,13 +1,7 @@
 // import Cookies from 'js-cookie'
 export default function({ isClient, isServer, store, req, route, error, redirect }) {
-  if (isServer) {
-    if (route.path !== '/login') {
-      redirect('/')
-    }
-  }
-  // 在客户端判读是否需要登陆
-  if (isClient) {
-    console.log('isClient..............')
+  if (!store.state.user.token) {
+    return redirect(`/`)
   }
 }
 
